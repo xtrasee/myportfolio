0efaf632cda5af9962a2c5b270db4958b1344b35
xtrasee <thn018@ucsd.edu>
2024-11-21 16:06:09 -0800

    turned config.php to .env for deployment

--- web_portfolio/mail.php
+++ web_portfolio/mail.php
@@ -11,23 +11,24 @@ use PHPMailer\PHPMailer\SMTP;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
-require 'config.php';
 
-//Create an instance; passing `true` enables exceptions
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 function sendMail($name, $email, $message){
     $mail = new PHPMailer(true);
 
     try {
         $mail->isSMTP();
         $mail->SMTPAuth   = true;
-        $mail->Host       = MAILHOST; 
-        $mail->Username   = USERNAME;                     //SMTP username
-        $mail->Password   = PASSWORD;                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Host       = getenv('MAILHOST'); 
+        $mail->Username   = getenv('USERNAME');                     
+        $mail->Password   = getenv('PASSWORD');                             
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            
         $mail->Port       = 587;  
     
-        $mail->setFrom(SEND_FROM, SEND_FROM_NAME);
-        $mail->addAddress(SEND_FROM); 
+        $mail->setFrom(getenv('SEND_FROM'), getenv('SEND_FROM_NAME'));
+        $mail->addAddress(getenv('SEND_FROM')); 
         $mail->addReplyTo($email, $name);
         $mail->isHTML(true); 
     
@@ -41,7 +42,7 @@ function sendMail($name, $email, $message){
 
     }catch (Exception $e){
         error_log('Mailer Error: ' . $mail->ErrorInfo);
-        return 'Mailer Error: ' . $e->ErrorInfo;
+        return 'Mailer Error: ' . $mail->ErrorInfo;
         }
 
     }
